aecff3d3b3287df9a97a7f38904a8536efc1317b
Mudit Kumar <115485952+mudit7011@users.noreply.github.com>
2025-06-15 13:07:23 +0530

    Update product.routes.js

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -7,6 +7,6 @@ const productRouter = express.Router();
 productRouter.post("/add-product",upload.single("image"), addProduct);
 productRouter.get("/get-products", getAllProducts);
 productRouter.put("/edit-product/:id", updateProduct);
-productRouter.put("/get-product/:id", getProductById);
+productRouter.get("/get-product/:id", getProductById);
 
 export default productRouter;
